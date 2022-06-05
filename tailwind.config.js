8deb8fc36f20c34a0897233d1582ac7610fd2269
Milic <milic@MacBook-Pro-som-tillhor-Milic.local>
2022-06-05 17:32:01 +0200

    static-site generation

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      heading: ['Poppins', 'sans-serif'],
+      body: ['Roboto', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
